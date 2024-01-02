f02ba8632817b2db842aa2e60dd47287d081665d
kareemAboelatta <kareemaboelatta@gmail.com>
2024-01-02 18:29:58 +0200

    Initial Commit

---
+++ tasks/tasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  tasksApp.swift
+//  tasks
+//
+//  Created by Baianat on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct tasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
